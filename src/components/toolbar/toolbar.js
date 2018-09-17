67be3c6fe4f51d6cb12733e55bef244a7d0d987b
Sheldon Santos <ssantos@thoughtworks.com>
2018-09-17 17:28:17 -0300

    added style for toolbar component

--- src/components/toolbar/toolbar.js
+++ src/components/toolbar/toolbar.js
@@ -1,11 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import styles from './toolbar.css';
 
-export default class toolbar extends Component {
+export default class Toolbar extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+       color: this.props.color,
+       title: this.props.title,
+    }
+  }
+  
+
   render() {
     return (
-      <div>
-        
-      </div>
+      <main style={{backgroundColor: this.state.color}} className={styles}>
+        <ul>
+          <li className={styles.title}>{this.state.title}</li>
+          <li className={styles.item}>item1</li>
+          <li className={styles.item}>item2</li>
+          <li className={styles.item}>item3</li>
+        </ul>
+      </main>
     )
   }
 }
