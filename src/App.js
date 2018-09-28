099beb2c584d48cac5f912831c687d6b844811a3
Sheldon Santos <ssantos@thoughtworks.com>
2018-09-28 14:40:53 -0300

    removed unecessary code in app component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
 import React, { Component } from 'react';
 import Toolbar from './components/toolbar/toolbar';
-import {Switch, Route} from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import Login from './components/login/login';
 import Home from './components/home/home';
+import Item from './components/toolbar/item/item';
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-  
-    this.state = {
-       
-    }
-  }
-  
+
   render() {
     return (
       <div className="App">
-      
-        <Toolbar title="title" color='red' />
+        <Toolbar title="title" color='lightblue'>
+          <Item name="home" route="/home"/>
+          <Item name="login" route="/login"/>
+        </Toolbar>
         <Switch>
-        <div>
-              <Route exact path="/home" component={Home} />
-              <Route exact path="/login" component={Login} />
-        </div>
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/login" component={Login} />
         </Switch>
       </div>
     );
--- src/components/home/home.js
+++ src/components/home/home.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react'
 import Card from '../card/card';
 
 export default class Home extends Component {
+  onclick = () => {
+    console.log('aqui');
+  }
+  
   render() {
     return (
       <div>
