b8f60afd3ca15d2c20c68fef5bdc2ea6824db982
Sheldon Santos <ssantos@thoughtworks.com>
2018-09-28 14:44:15 -0300

    added item inside toolbar

---
+++ src/components/toolbar/item/item.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from "react-router-dom";
+import styles from './item.css';
+
+const item = (props) => (
+    <Link className={styles.item} to={props.route}>{props.name}</Link>
+);
+
+item.propTypes = {
+    name: PropTypes.string,
+    route: PropTypes.string
+}
+
+export default item;
